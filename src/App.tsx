24c423cc3741a7cdd21d3920825ddbbcc00c7011
Lindsay Ferreira <eson@iouu.com.br>
2020-11-24 14:43:42 -0300

    feat: atualizar componentes app e index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+
+import Body from "./components/Body";
 import Header from "./components/Header";
+
+import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Header />
+      <Body />
     </div>
   );
 }
