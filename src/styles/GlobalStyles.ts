3d3b01a80a7c51de9e241b2357ebbb2f7753eb38
Lindsay Ferreira <eson@iouu.com.br>
2020-11-24 14:41:46 -0300

    feat: criar componente de estilos globais

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+// FONTS
+
+const GlobalStyle = createGlobalStyle` 
+  * {    
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
+
+export default GlobalStyle;
