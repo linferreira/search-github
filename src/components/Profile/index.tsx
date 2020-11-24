0f87f7b9902cec8787df28a8e5385ed0834a6065
Lindsay Ferreira <eson@iouu.com.br>
2020-11-24 15:08:35 -0300

    feat: atualizar componente de perfil

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -5,11 +5,19 @@ import { Container } from "./styles";
 interface IProfileProps {
   userAvatarUrl: string;
   userName: string;
+  userProfile: string;
 }
 
-const Profile: React.FC<IProfileProps> = ({ userAvatarUrl, userName }) => (
+const Profile: React.FC<IProfileProps> = ({
+  userAvatarUrl,
+  userName,
+  userProfile,
+}) => (
   <Container>
-    <img src={userAvatarUrl} alt="avatar" />
+    <a href={userProfile} target="_blank">
+      <img src={userAvatarUrl} alt="avatar" />
+    </a>
+    <p>{userName}</p>
   </Container>
 );
 
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+
+  > a img {
+    height: 100px;
+    border-radius: 8px;
+  }
+
+  > p {
+    margin-top: 10px;
+    margin-left: 10px;
+  }
+`;
