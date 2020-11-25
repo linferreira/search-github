bc21113eb3aabb9db2db2888e50efc0e20009802
Lindsay Ferreira <eson@iouu.com.br>
2020-11-25 16:48:48 -0300

    feat: atualizar o nome da pagina

---
+++ src/page/Content/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import * as ReactRedux from "react-redux";
+
+import Body from "../../components/Body";
+import Header from "../../components/Header";
+import Spinner from "../../components/Spinner";
+
+import { IAppState } from "../../redux/configureStore";
+
+const Content = () => {
+  const usersState = ReactRedux.useSelector(
+    (reducers: IAppState) => reducers.getUsers
+  );
+
+  if (usersState.loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <Header />
+      <Body total={usersState.total} users={usersState.users} />
+    </>
+  );
+};
+
+export default Content;
