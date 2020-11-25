47f81f57b0812f089f64e5b9d8ba775a0bc83d12
Lindsay Ferreira <eson@iouu.com.br>
2020-11-25 18:56:50 -0300

    feat: refatorar componente de body

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,56 +1,29 @@
 import React from "react";
-import * as ReactRedux from "react-redux";
-
-import { IAppState } from "../../redux/configureStore";
 
 import Profile from "../Profile";
 
 import { Container } from "./styles";
 
 interface IBodyProps {
-  total: number;
   users: any[];
+  message: string;
 }
 
-const Body: React.FC<IBodyProps> = () => {
-  const usersState = ReactRedux.useSelector(
-    (reducers: IAppState) => reducers.getUsers
-  );
+const Body: React.FC<IBodyProps> = ({ users, message }) => (
+  <Container>
+    <p> {message} </p>
 
-  function showMessage() {
-    if (usersState.total === 0 && usersState.username !== "") {
-      return <p className="not-found">User not found!</p>;
-    } else if (usersState.username !== "") {
-      return (
-        <p>
-          Found {usersState.total} results from <b>{usersState.username}</b>
-        </p>
-      );
-    } else if (usersState.error) {
-      return (
-        <p className="error">
-          An error ocurred in your request, please try again
-        </p>
-      );
-    }
-  }
-
-  return (
-    <Container>
-      {showMessage()}
-
-      <div>
-        {usersState.users.map((item: any) => (
-          <Profile
-            key={item.login}
-            userAvatarUrl={item.avatar_url}
-            userName={item.login}
-            userProfile={item.html_url}
-          />
-        ))}
-      </div>
-    </Container>
-  );
-};
+    <div>
+      {users.map((item: any) => (
+        <Profile
+          key={item.login}
+          userAvatarUrl={item.avatar_url}
+          userName={item.login}
+          userProfile={item.html_url}
+        />
+      ))}
+    </div>
+  </Container>
+);
 
 export default Body;
--- src/components/Body/styles.ts
+++ src/components/Body/styles.ts
@@ -10,19 +10,8 @@ export const Container = styled.div`
     background-color: #f7f7ad;
     align-self: center;
     margin-bottom: 30px;
-  }
-
-  .not-found {
-    background-color: transparent;
-    font-size: 24px;
-    color: #e07408;
-    font-weight: 600;
-  }
-
-  .error {
-    background-color: transparent;
-    font-size: 24px;
-    color: #e44c4e;
+    padding: 8px;
+    border-radius: 5px;
   }
 
   > div {
