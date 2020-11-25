935cb43cd739c90f60fc283ab74732a9bb93c76a
Lindsay Ferreira <eson@iouu.com.br>
2020-11-25 16:51:19 -0300

    feat: adicionar redux no componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
+import * as ReactRedux from "react-redux";
 
 import Input from "../Input";
 import Title from "../Title";
 
 import { Container } from "./styles";
+import { getUsersAction } from "../../redux/reducers/search/index";
 
 const Header = () => {
+  let typingTimer: any = null;
+  const doneTypingInterval = 1000;
+
+  const dispatch = ReactRedux.useDispatch();
   const [user, setUser] = useState("");
+
+  function onkeyup() {
+    clearTimeout(typingTimer);
+    if (user) {
+      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+  }
+
+  function onkeydown() {
+    clearTimeout(typingTimer);
+  }
+
+  function doneTyping() {
+    dispatch(getUsersAction(user));
+  }
+
+  function handleChange(value: string) {
+    setUser(value);
+  }
 
   return (
     <Container>
@@ -14,8 +39,10 @@ const Header = () => {
         <Title />
         <Input
           placeholder="User"
-          onChange={({ target }) => setUser(target.value)}
+          onChange={({ target }) => handleChange(target.value)}
           required
+          onKeyUp={onkeyup}
+          onKeyDown={onkeydown}
         />
       </div>
     </Container>
